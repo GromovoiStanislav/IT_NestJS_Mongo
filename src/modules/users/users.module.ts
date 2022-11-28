a7744ac002acc94ece6678bc42e3df9dda525634
Gromovoi Stanislav <gromila12@rambler.ru>
2022-11-28 14:53:18 +0600

    refactoring users

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,17 +2,17 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UsersController } from "./users.controller";
 import {
-  ClearAllUsersCommand,
+  ClearAllUsersUseCase,
   CreateUserUseCase,
   DeleteUserUseCase,
-  FindAllUsersUseCase,
+  FindAllUsersUseCase
 } from "./users.service";
 import { User, UserSchema } from "./schemas/users.schema";
 import { UsersRepository } from "./users.repository";
 import { CqrsModule } from "@nestjs/cqrs";
 
 const useCases = [
-  ClearAllUsersCommand,
+  ClearAllUsersUseCase,
   FindAllUsersUseCase,
   DeleteUserUseCase,
   CreateUserUseCase
