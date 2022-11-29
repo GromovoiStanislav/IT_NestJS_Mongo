5410329ce1cdeba622d3ab9e14072cb8b9a2614a
Gromovoi Stanislav <gromila12@rambler.ru>
2022-11-29 11:03:32 +0600

    auth/registration-confirmation

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { CqrsModule } from "@nestjs/cqrs";
-import { RegisterUserUseCase, ResendConfirmationCodeUseCase } from "./auth.service";
+import { ConfirmEmailUseCase, RegisterUserUseCase, ResendConfirmationCodeUseCase } from "./auth.service";
 import { UsersModule } from "../users/users.module";
 import { EmailAdapter } from "../../utils/email-adapter";
 import { Settings } from "../../settings";
 
 const useCases= [
   RegisterUserUseCase,
-  ResendConfirmationCodeUseCase
+  ResendConfirmationCodeUseCase,
+  ConfirmEmailUseCase
 ]
 
 
