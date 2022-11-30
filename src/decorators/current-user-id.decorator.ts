638b844f87adc439c0b4cab09d9841c4c373947f
Gromovoi Stanislav <gromila12@rambler.ru>
2022-11-30 16:03:19 +0600

    posts/postId/like-status

--- src/decorators/current-user-id.decorator.ts
+++ src/decorators/current-user-id.decorator.ts
@@ -3,9 +3,9 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 export const CurrentUserId = createParamDecorator(
   (data: unknown, context: ExecutionContext): number => {
     const request = context.switchToHttp().getRequest();
-    if (!request.userid) {
+    if (!request.userId) {
       throw Error("BearerAuthGuard must be used");
     }
-    return request.userid;
+    return request.userId;
   }
 );
