8fca4ec0cd5bb62aa6d6a172f5c61a918ed9124b
Gromovoi Stanislav <stasgromovoi@gmail.com>
2022-12-03 13:01:09 +0000

    +cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { BadRequestException, ValidationPipe } from "@nestjs/common";
 import { ErrorExceptionFilter, HttpExceptionFilter } from "./exception.filter";
 import { ConfigService } from "@nestjs/config";
 import { useContainer } from "class-validator";
-import {cookieParser} from 'cookie-parser;
+import cookieParser from 'cookie-parser;
 //import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
