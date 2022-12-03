40a868c6de9c5f3cee7ef905e8a43e08349465a6
Gromovoi Stanislav <stasgromovoi@gmail.com>
2022-12-03 12:40:40 +0000

    +cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { BadRequestException, ValidationPipe } from "@nestjs/common";
 import { ErrorExceptionFilter, HttpExceptionFilter } from "./exception.filter";
 import { ConfigService } from "@nestjs/config";
 import { useContainer } from "class-validator";
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
