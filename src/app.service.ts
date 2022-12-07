356b723710f24a02bd8b423b65119b9029c43b40
Gromovoi Stanislav <gromila12@rambler.ru>
2022-12-07 15:02:54 +0600

    sa/users/id/ban

--- src/app.service.ts
+++ src/app.service.ts
@@ -16,6 +16,7 @@ export class AppService {
     //const MONGO_URI = this.configService.get("MONGO_URI");
 
     //return "PORT: " + this.configService.get("PORT");
-    return "HELP!!!";
+    return process.env.PORT;
+    //return "HELP!!!";
   }
 }
